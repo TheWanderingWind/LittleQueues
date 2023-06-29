bf94db309dc5c694a2fce19998a056ac5b28529a
TheWanderingWind <mr.tarnado4@gmail.com>
2023-06-29 23:59:47 +0300

    Add get/add functions for queues and users

--- pyServer.py
+++ pyServer.py
@@ -1,11 +1,61 @@
 from pymongo import MongoClient
 from flask import Flask
+import uuid
 
 app = Flask(__name__)
 client = None
 db = None
 queues = None
 users = None
+
+# Add new user
+def add_user(name, cookies=[]):
+    new_user = {
+        'name': name,
+        'cookies': cookies,
+    }
+    result = users.insert_one(new_user)
+    new_user['id'] = result.inserted_id
+    return new_user
+
+
+# Add new queue
+def add_queue(name, hostId, description):
+    collection_queues = queues.find()
+    new_queue = {
+        'id': len(collection_queues),
+        'name': name,
+        'hostId': hostId,
+        'description': description,
+        'queue': [],
+        'finishedQueue': [],
+        'currentParticipantId': None,
+        'status': 'active'
+    }
+    queues.insert_one(new_queue)
+
+
+# Get user data by id
+def get_user_by_id(id):
+    collection_user = users.find()
+    user_data = None
+    for item in collection_user:
+        if item['id'] == id:
+            user_data = item
+            break
+    return user_data
+
+
+# Get queue data by id
+def get_queue_by_id(id):
+    collection_queues = queues.find()
+    queue_data = None
+    for item in collection_queues:
+        if item['id'] == id:
+            queue_data = item
+            break
+    return queue_data
+
 
 # Main site
 @app.route('/')
@@ -23,8 +73,8 @@ if __name__ == '__main__':
     client = MongoClient('mongodb://localhost:27017/')
     db = client['QueuesData']
 
-    queues = db['queues'].find()
-    users = db['users'].find()
+    queues = db['queues']
+    users = db['users']
 
     for document in queues:
         print(document)
