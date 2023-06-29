e65c292cb905ce43299f0731a15a24452adc829b
TheWanderingWind <mr.tarnado4@gmail.com>
2023-06-29 22:52:43 +0300

    Add lib for request and first test request.

--- pyServer.py
+++ pyServer.py
@@ -1,20 +1,34 @@
 from pymongo import MongoClient
+from flask import Flask
 
-# Підключення до MongoDB сервера
-client = MongoClient('mongodb://localhost:27017/')
+app = Flask(__name__)
+client = None
+db = None
+queues = None
+users = None
 
-# Вибір бази даних
-db = client['QueuesData']
+# Main site
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
-# Вибір колекції
-collection = db['queues']
 
-# Виконання запиту на вибірку даних
-result = collection.find()
+# Here we will get user id
+@app.before_request
+def setup():
+    pass
 
-# Виведення результатів
-for document in result:
-    print(document)
 
-# Закриття з'єднання
-client.close()
+if __name__ == '__main__':
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['QueuesData']
+
+    queues = db['queues'].find()
+    users = db['users'].find()
+
+    for document in queues:
+        print(document)
+
+    print('Сервер запущено')
+
+    app.run()
